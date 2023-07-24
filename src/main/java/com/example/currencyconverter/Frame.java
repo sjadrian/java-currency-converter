ea5eb33547472f1708d07dea0d3cb6ec498fd2b0
Stefanus Adrian <stefanusj06@gmail.com>
2023-07-24 15:58:08 +0700

    update frame

--- src/main/java/com/example/currencyconverter/Frame.java
+++ src/main/java/com/example/currencyconverter/Frame.java
@@ -3,9 +3,11 @@ package com.example.currencyconverter;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class Frame extends Application {
     @Override
@@ -14,6 +16,10 @@ public class Frame extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(Frame.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Currency Converter");
+
+        Image image = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/dollar-logo.jpg")));
+        stage.getIcons().add(image);
+
         stage.setScene(scene);
         stage.setResizable(false);
         stage.show();
